504a68581ba520dca817dd2725f21d90610aaada
Denys Smirnov <denis.smirnov.91@gmail.com>
2018-04-18 10:21:21 +0200

    kv: add package that imports all available drivers

---
+++ kv/all/all.go
@@ -0,0 +1,6 @@
+package all
+
+import (
+	_ "github.com/nwca/hidalgo/kv/bolt"
+	_ "github.com/nwca/hidalgo/kv/flat/all"
+)
---
+++ kv/flat/all/all.go
@@ -0,0 +1,7 @@
+package all
+
+import (
+	_ "github.com/nwca/hidalgo/kv/flat/badger"
+	_ "github.com/nwca/hidalgo/kv/flat/btree"
+	_ "github.com/nwca/hidalgo/kv/flat/leveldb"
+)
