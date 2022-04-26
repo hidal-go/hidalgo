c56850fbfc2bc5790c98139d958fec62e94d543d
Oliver <0uep@hessling.fr>
2022-04-26 17:19:21 +0200

    Rename types treePool and enumPool

--- kv/flat/btree/keys.go
+++ kv/flat/btree/keys.go
@@ -79,22 +79,22 @@ func init() {
 
 var (
 	btDPool = sync.Pool{New: func() interface{} { return &d{} }}
-	btEPool = btEpool{sync.Pool{New: func() interface{} { return &Enumerator{} }}}
-	btTPool = btTpool{sync.Pool{New: func() interface{} { return &Tree{} }}}
+	btEPool = enumPool{sync.Pool{New: func() interface{} { return &Enumerator{} }}}
+	btTPool = treePool{sync.Pool{New: func() interface{} { return &Tree{} }}}
 	btXPool = sync.Pool{New: func() interface{} { return &x{} }}
 )
 
-type btTpool struct{ sync.Pool }
+type treePool struct{ sync.Pool }
 
-func (p *btTpool) get(cmp Cmp) *Tree {
+func (p *treePool) get(cmp Cmp) *Tree {
 	x := p.Get().(*Tree)
 	x.cmp = cmp
 	return x
 }
 
-type btEpool struct{ sync.Pool }
+type enumPool struct{ sync.Pool }
 
-func (p *btEpool) get(err error, hit bool, i int, k []byte, q *d, t *Tree, ver int64) *Enumerator {
+func (p *enumPool) get(err error, hit bool, i int, k []byte, q *d, t *Tree, ver int64) *Enumerator {
 	x := p.Get().(*Enumerator)
 	x.err, x.hit, x.i, x.k, x.q, x.t, x.ver = err, hit, i, k, q, t, ver
 	return x
